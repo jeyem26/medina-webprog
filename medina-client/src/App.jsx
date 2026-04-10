25bbe4bd35a353bedc671a64f9d3aa1928052ec9
Your Name <you@example.com>
2026-04-10 15:15:55 +0800

    Update App.jsx

--- medina-client/src/App.jsx
+++ medina-client/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
 
-funtion App() {
+function App() {
   return (
     <div className="App">
-      
+      <header className="App-header">
+        <h1>Welcome to My React App!</h1>
+        <p>
+          Name: jeyem26<br />
+          Email: medina.jeyem@gmail.com<br />
+          Other Personal Info:
+          <a href="http:https://github.com/jeyem26/medina-webprog.git"></a>
+        </p>
+      </header>
     </div>
-  )
+  );
 }
+
+export default App;
